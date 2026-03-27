308d7e61cb5a6e04f39f4a3521c47c250b58b302
Pavel Hrách <pavel@hrach.email>
2026-03-27 21:24:58 +0100

    update outdated packages, cleanup unnecessary deploy script

--- examples/react-example/vite.config.ts
+++ examples/react-example/vite.config.ts
@@ -2,16 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  run: {
-    tasks: {
-      netlify: {
-        // On Netlify `vp build` fails with:
-        // Failed to spawn process: failed to create IPC channel: Creating the shared memory failed, os error 2
-        command: "vite build",
-        dependsOn: ["@fiveway/core#build", "@fiveway/react#build", "@fiveway/devtools#build"],
-      },
-    },
-  },
   build: { minify: false, sourcemap: true },
   plugins: [react()],
 });
