579d080364c0e03d6cb81752815762b5e5d0e629
Pavel Hrách <pavel@hrach.email>
2026-03-27 20:30:46 +0100

    fix broken build and duplicate react in react example

--- examples/react-example/vite.config.ts
+++ examples/react-example/vite.config.ts
@@ -1,8 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite-plus";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // build: { minify: false },
+  run: {
+    tasks: {
+      build: {
+        command: "vp build",
+        dependsOn: ["@fiveway/core#build", "@fiveway/react#build", "@fiveway/devtools#build"],
+      },
+    },
+  },
+  build: { minify: false, sourcemap: true },
   plugins: [react()],
 });
