ca1cf1bc7e6bb1b55154cfe98fc1ab13d06f513e
Pavel Hrách <pavel@hrach.email>
2026-03-27 20:43:46 +0100

    try `vite build` instead of `vp build` since that fail on Netlify `#

--- examples/react-example/vite.config.ts
+++ examples/react-example/vite.config.ts
@@ -4,8 +4,10 @@ import { defineConfig } from "vite-plus";
 export default defineConfig({
   run: {
     tasks: {
-      build: {
-        command: "vp build",
+      netlify: {
+        // On Netlify `vp build` fails with:
+        // Failed to spawn process: failed to create IPC channel: Creating the shared memory failed, os error 2
+        command: "vite build",
         dependsOn: ["@fiveway/core#build", "@fiveway/react#build", "@fiveway/devtools#build"],
       },
     },
