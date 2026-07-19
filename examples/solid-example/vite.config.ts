cce1ed64c7320fdefc547fbab39376de6cefe657
Pavel Hrách <pavel@hrach.email>
2026-07-19 17:22:18 +0200

    allow configuring inspector URL in examples

--- examples/solid-example/vite.config.ts
+++ examples/solid-example/vite.config.ts
@@ -1,5 +1,9 @@
+import { env } from "node:process";
+
 import solid from "vite-plugin-solid";
 import { defineConfig } from "vite-plus";
+
+console.log(env.FIVEWAY_INSPECTOR_URL);
 
 export default defineConfig({
 	build: { minify: false, target: "esnext" },
@@ -7,5 +11,8 @@ export default defineConfig({
 	server: { port: 3001 },
 	define: {
 		"import.meta.env.FIVEWAY_INSPECTOR": "true",
+		"import.meta.env.FIVEWAY_INSPECTOR_URL": JSON.stringify(
+			env.FIVEWAY_INSPECTOR_URL ?? "http://localhost:3030",
+		),
 	},
 });
