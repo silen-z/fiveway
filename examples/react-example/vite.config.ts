db8cfe52d254432b8ef5527a430c42b258007525
Pavel Hrách <pavel@hrach.email>
2026-07-19 17:22:18 +0200

    allow configuring inspector URL in examples

--- examples/react-example/vite.config.ts
+++ examples/react-example/vite.config.ts
@@ -1,3 +1,5 @@
+import { env } from "node:process";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite-plus";
 
@@ -7,6 +9,9 @@ export default defineConfig({
 	server: { port: 3000 },
 	define: {
 		"import.meta.env.FIVEWAY_INSPECTOR": "true",
+		"import.meta.env.FIVEWAY_INSPECTOR_URL": JSON.stringify(
+			env.FIVEWAY_INSPECTOR_URL ?? "http://localhost:3030",
+		),
 	},
 	lint: {
 		plugins: ["react"],
